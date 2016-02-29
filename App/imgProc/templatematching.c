1d3244505131a3b54fc21424726621a49aecac10
Indumathi Duraipandian <indumathi.duraipandian@student.fh-kiel.de>
2016-02-29 09:53:17 +0100

    image processing utils

--- App/imgProc/templatematching.c
+++ App/imgProc/templatematching.c
@@ -5,106 +5,69 @@
  *      Author: Indumathi Duraipandian
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "templatematching.h"
 
 uint8 imageData[TEMPLATEMATCHING_IMAGE_LENGTH];
 
+uint8 silentRun = 1;
+uint8 showImages = 0;
+
 int main(int argc, char* argv[])
 {
-	char inputFileName[100];
-	FILE* inputFile = NULL;
-	uint32 fileLength = 0;
-	uint32 numBytesRead = 0;
+  struct bmpInfo imgHdr;
+  int numObjs = 0;
+  
+  /* If enough inputs available */
+	if(argc >= 2) {
+      /* Open the given BMP file  */
+      if(openBmpFile(&argv[1][0], &imgHdr, &imageData[0]) == 0) {
+        /* Convert the read RGB file into gray scale and put it in a Matrix */
+        PMAT gray = convertRGBToGrayScale(imageData, imgHdr.imageLength,
+                                                     imgHdr.imageWidth,
+                                                     imgHdr.imageHeight);
+        PMAT opgray = convertRGBToGrayScale(imageData, imgHdr.imageLength,
+                                                       imgHdr.imageWidth,
+                                                       imgHdr.imageHeight);
 
-	uint32 imageWidth = 0, imageHeight = 0;
+        /* Begin image processing */
+        PMAT contrastImg = imadjust(gray, 60, 180, 255, 0, 1); destroyMatrix(gray, "gray");
+        PMAT noBgImg = removeBG(contrastImg);  destroyMatrix(contrastImg, "contrastImg");
+        PMAT blurImg = imblur(noBgImg);  destroyMatrix(noBgImg, "noBgImg");
+        //PMAT sharpImg = imsharpen(blurImg); destroyMatrix(blurImg, "blurImg1");
+        //PMAT adjustedImg = imadjust(sharpImg, 0, 176, 0, 255, 1); destroyMatrix(sharpImg, "sharp1");
+        //sharpImg = imsharpen(adjustedImg); destroyMatrix(adjustedImg, "adjust1");
+        //adjustedImg = imadjust(sharpImg, 0, 210, 0, 255, 1); destroyMatrix(sharpImg, "sharp2");
+        float threshold = quantile(blurImg, 0.45); 
+        printf("Threshold = %f\n", threshold);
+        
+        PMAT binImage = binarizeImage(blurImg, threshold); destroyMatrix(blurImg, "blurImg");
+        thickImage(binImage);
+        
+        //PMAT opImage = imageFromBinInfo(binImage);
+        PMAT opImage = seperateObjsFromImage(binImage, &numObjs); destroyMatrix(binImage, "thickbin");
+        
+        PMAT opObjs = markObjsInImage(opgray, opImage, numObjs); destroyMatrix(opImage, "opImage");
+        
+        //dumpMatrixToFile(opImage, "image.txt", "%05.0f ", "      ");
+        writeAsBMP("output.bmp", opObjs, &imageData[0]);
+        /* Write sharpened image as output */
+        destroyMatrix(opgray, "");
+        destroyMatrix(opObjs, "");
+      }
+    }
+    /* In case no inputs, go to test mode */
+    else {
+      /* Do test operations */
+      doTest();
+    }
+  
+  system("cygstart ./output.bmp");
 
-	if(argc >= 1) {
-		strcpy(inputFileName, argv[1]);
-		imageWidth = atoi(argv[2]);
-		imageHeight = atoi(argv[3]);
-	}
-
-	inputFile = fopen(inputFileName, "rb");
-
-	if (inputFile != NULL) {
-		fseek(inputFile, 0, SEEK_END);
-		fileLength = ftell(inputFile);
-		fseek(inputFile, 0, SEEK_SET);
-
-		numBytesRead = fread(imageData, 1, fileLength, inputFile);
-
-		fclose(inputFile);
-	}
-
-	struct matrix* gray = convertRGBToGrayScale(imageData,numBytesRead,imageWidth,imageHeight);
-
-	fileLength = convertMatrixToBuffer(gray,imageData);
-
-	FILE* outputFile = fopen("output.bmp", "wb");
-
-		if (outputFile != NULL) {
-			fwrite(imageData,fileLength,1,outputFile);
-			fclose(outputFile);
-		}
-
-
-	//printMatrix(gray);
-
-	// Declare matrices for image,filter and the output
-	struct matrix* image;
-	struct matrix* filter;
-	struct matrix* filteredImage;
-
-	//Intialise matrices
-	image = createMatrix(7,7);
-	filter = createMatrix(5,5);
-
-	float value = 1.0;
-
-	// For testing purpose - Initialised a 7x7 matrix as image
-	for (uint8 i = 0; i < image->numberOfRows; ++i) {
-		for (uint8 j = 0; j < image->numberOfColumns; ++j) {
-			MAT(image, i, j) = value;
-			value++;
-		}
-	}
-
-#ifdef DEBUG
-	printMatrix(image,"Loaded image");
-#endif
-
-	// For testing purpose - Initialised a 5x5 matrix as filter mask
-	float filterValues[5][5] = {{1.0, 1.0, 1.0,  1.0, 1.0},
-								{1.0, 1.0, 1.0,  1.0, 1.0},
-								{1.0, 1.0,-24.0, 1.0, 1.0 },
-								{1.0, 1.0, 1.0,  1.0, 1.0},
-								{1.0, 1.0, 1.0,  1.0, 1.0}};
-
-	initMatrix(filter, (void*)filterValues);
-
-	// Apply filter
-	// operation - Convolution/Correlation
-	// Mode - by which mode the values outside the array bound is computed
-	// Mode - Zero padding/Symmetric/Circular/Replicate
-	filteredImage = imfilter(image,filter,CORRELATION_OPERATION,MODE_CIRCULAR);
-
-	filteredImage = imsharpen(filteredImage);
-
-	printMatrix(filteredImage,"Sharpened image");
-
-	struct matrix* adjustedImage = imadjust(filteredImage,0,0.69,0,1,1);
-
-	printMatrix(adjustedImage,"Adjusted image");
-
-	destroyMatrix(image);
-	destroyMatrix(filter);
-	destroyMatrix(filteredImage);
-	destroyMatrix(adjustedImage);
-
+  if(silentRun == 0) {
+    printf("\n Enter any key to close\n ");
+    getchar();
+  }
+  
 	return 0;
 }
 
--- App/imgProc/templatematching.h
+++ App/imgProc/templatematching.h
@@ -8,12 +8,14 @@
 #ifndef TEMPLATEMATCHING_H_
 #define TEMPLATEMATCHING_H_
 
-#include "Utilities/misc.h"
-#include "Utilities/imfilter.h"
-#include "Utilities/matrix.h"
-#include "Utilities/RGBToGray.h"
+#include "utils/misc.h"
+#include "utils/matrix.h"
+#include "fileOps/bmp.h"
+#include "imgOps/imfilter.h"
+#include "imgOps/imgUtils.h"
+#include "imgOps/RGBToGray.h"
+#include "test/test.h"
 
-
-#define TEMPLATEMATCHING_IMAGE_LENGTH (4000000)
+#define TEMPLATEMATCHING_IMAGE_LENGTH (10000000)
 
 #endif /* TEMPLATEMATCHING_H_ */
